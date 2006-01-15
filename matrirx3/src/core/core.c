decf5fa39267b1feeb5be68aa9ff16be7720ec52
Mark Karpeles <magicaltux@gmail.com>
2006-01-15 11:03:06 +0000

    Minor modifications

--- matrirx3/src/core/core.c
+++ matrirx3/src/core/core.c
@@ -1,5 +1,6 @@
 /* core.c
  * This is the core of the core !
+ * $Id$
  */
 
 #include <stdio.h>
@@ -10,9 +11,9 @@
 //int zend_eval_string_ex(char *str, void *, char *string_name, int handle_exceptions);
 
 int main(int argc, char *argv[]) {
-	char *phpcode="echo 'Hello World from PHP/'.phpversion().' MatrIRX/'.matrirx_version().' - SAPI='.php_sapi_name().\"\\n\";";
+	char *phpcode="echo 'Hello World from PHP/'.phpversion().' MatrIRX/'.matrirx_version().' - SAPI='.php_sapi_name().\"\\n\"; var_dump($_SERVER);";
 	global_executable_location = argv[0];
-	printf("Passing PHP code to PHP5(sapi:embed): %s\n", phpcode);
+	printf("Passing PHP code to PHP5(sapi:matrirx): %s\n", phpcode);
 	php_matrirx_sapi_init(0, NULL);
 	zend_eval_string_ex(phpcode, NULL, "Test Code", 1);
 	php_matrirx_sapi_shutdown();
